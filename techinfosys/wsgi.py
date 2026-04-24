73e28a76936049188b0abcfa945e9a339fcecbb0
Ritesh <riteshcdy12@gmail.com>
2026-04-24 15:06:41 +0545

    techinfosys django project

--- techinfosys/wsgi.py
+++ techinfosys/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neptech_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techinfosys.settings')
 
 application = get_wsgi_application()
